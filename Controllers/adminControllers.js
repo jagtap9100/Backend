1357d3b2f22b41f6e10609cfa92b90cc977bfca4
Abhi jagtap <151128024+jagtap9100@users.noreply.github.com>
2025-01-01 21:51:59 +0530

    Changes to be committed

--- Controllers/adminControllers.js
+++ Controllers/adminControllers.js
@@ -1,6 +1,10 @@
 const adminlogin = (req, res) => {
   // Code to validate admin credentials and grant access to admin routes
   res.json({ message: "Admin login successful!" });
+  res.json({
+    message:
+      "Code to validate admin credentials and grant access to admin routes!",
+  });
 };
 
 module.exports = { adminlogin };
